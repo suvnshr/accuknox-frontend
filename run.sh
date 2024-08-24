63c0e6bd1cb22e38efb4b15f486ebf43670fb664
Suvansh <36293610+suvnshr@users.noreply.github.com>
2024-08-24 16:04:48 +0530

    chore(docker): add a command to remove any running containers

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
+# Remove any existing running container 
+docker rm -f accuknox-frontend
+
 # Build a new docker image
-docker build -t accuknox-frontend .
+docker build -t accuknox-frontend . --no-cache
 
 # Run the docker image
 
-docker run -d -p 3101:3000 accuknox-frontend
+docker run -d -p 3101:3000 --name accuknox-frontend accuknox-frontend
